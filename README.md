b1c751d13bcb30ae4449c51542135604ac01b36c
ZevonikRF <rifky.fachrizain@gmail.com>
2024-12-14 23:59:07 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# ABC's Table & Queue Management Dashboard
+Dashboard ini akan menampilkan daftar meja yang tersedia dan juga daftar antrian pada Cafe ABC. Dashboard ini diharapkan dapat membantu keefektifan kerja dari Cafe ABC dalam manajemen meja dan juga antrian agar dapat meminimalisir ketidakpuasan pelanggan Cafe ABC.
+
+## How to use
+Coming Soon >∇<
+
+### Proyek Tugas Besar II3160 Teknologi Sistem Terintegrasi
+18221027 Muhammad Rifky Fachrizain
+
+### SIDENOTE
+Proyek dipindahkan dari repo [sebelumnya]https://github.com/ZevonikRF/tugas-besar-ii3160. dikarenakan ada masalah integrasi
+
+Proyek ini akan menggunakan bahasa PHP.
+
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Cafe ABC</title>
+    <style>
+        :root {
+            --background: #1f2170;
+            --backgroundSd: #272987;
+            --foreground: #171717;
+            --text: white;
+        }
+        body{
+            background-color: var(--background);
+        }
+        .container{
+            display: flex;
+            width: 100%;
+            height: 100vh;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+        }
+        .title{
+            font-size: 2rem;
+            font-weight: 700;
+            color: var(--text);
+        }
+        a{
+            padding: 20px 50px 20px;
+            margin-top: 20px;
+            background-color: white;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="title">Cafe ABC Dashboard</div>
+        <a href="login/login.php">Login</a>
+    </div>
+</body>
+</html>
